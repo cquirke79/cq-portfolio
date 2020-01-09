8a6b61a2a5fd23fd005108cc9d97c575a3d6f1d0
Chris Quirke <Kim@MacBook-Pro.local>
2020-01-10 11:54:08 +1300

    Upload Portfolio Python script updated

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,11 +4,15 @@ import StringIO
 import zipfile
 import mimetypes
 
+# Using the boto 3 resource command
 s3 = boto3.resource('s3',config = Config(signature_version='s3v4'))
 portfolio_bucket=s3.Bucket('portfolio.quichenews.net')
 build_bucket=s3.Bucket('portfoliobuild.quichenews.net')
 
+#String IO just holds a file in memory
 portfolio_zip=StringIO.StringIO()
+
+# Downloads the file 'portfoliobuild.zip' from the build_bucket and loads it into the String IO param portfolio_zip
 build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -17,5 +21,7 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
         portfolio_bucket.upload_fileobj(obj,nm,
             ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0],
             'ACL':'public-read'})
+#ExtraArgs here just sets all the objects in the zip file to public and also sets the right Mime type depending upon the file extension
+
         #portfolio_bucket.Object(nm).Acl().put(ACL='public_read')
 #{}
