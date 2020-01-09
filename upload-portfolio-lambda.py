afd7560d2ae7198797326a4eb570fadefaec9c3d
Chris Quirke <Kim@MacBook-Pro.local>
2020-01-10 12:14:49 +1300

    Lambda code for Upload-Lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,24 +4,29 @@ import StringIO
 import zipfile
 import mimetypes
 
-# Using the boto 3 resource command
-s3 = boto3.resource('s3',config = Config(signature_version='s3v4'))
-portfolio_bucket=s3.Bucket('portfolio.quichenews.net')
-build_bucket=s3.Bucket('portfoliobuild.quichenews.net')
+def lambda_handler(event, context):
 
-#String IO just holds a file in memory
-portfolio_zip=StringIO.StringIO()
 
-# Downloads the file 'portfoliobuild.zip' from the build_bucket and loads it into the String IO param portfolio_zip
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    # Using the boto 3 resource command
+    s3 = boto3.resource('s3',config = Config(signature_version='s3v4'))
+    portfolio_bucket=s3.Bucket('portfolio.quichenews.net')
+    build_bucket=s3.Bucket('portfoliobuild.quichenews.net')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj= myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0],
-            'ACL':'public-read'})
-#ExtraArgs here just sets all the objects in the zip file to public and also sets the right Mime type depending upon the file extension
+    #String IO just holds a file in memory
+    portfolio_zip=StringIO.StringIO()
+
+    # Downloads the file 'portfoliobuild.zip' from the build_bucket and loads it into the String IO param portfolio_zip
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj= myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0],
+                'ACL':'public-read'})
+    #ExtraArgs here just sets all the objects in the zip file to public and also sets the right Mime type depending upon the file extension
+
+    return 'Files Uploaded'
 
         #portfolio_bucket.Object(nm).Acl().put(ACL='public_read')
 #{}
